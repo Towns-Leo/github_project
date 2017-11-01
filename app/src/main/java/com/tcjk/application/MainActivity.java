109acffdeb0ac3231ceb3146da811024ecf1ed14
TCJK-LT <ltxxx@126.com>
2017-11-01 15:14:15 +0800

    first commit

--- app/src/main/java/com/tcjk/application/MainActivity.java
+++ app/src/main/java/com/tcjk/application/MainActivity.java
@@ -2,12 +2,20 @@ package com.tcjk.application;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+
+    @BindView(R.id.text_view)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
