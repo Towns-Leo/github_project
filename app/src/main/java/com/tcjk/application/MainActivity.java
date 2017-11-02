c4771ee5b8859d4c477566c4d0b61d429b85051e
TCJK-LT <ltxxx@126.com>
2017-11-02 14:15:33 +0800

    Revert "Revert "Merge pull request #3 from Towns-Leo/testBranch2""
    This reverts commit 67cf99e63d81b056f43060ec8dcbb9957542972d.

--- app/src/main/java/com/tcjk/application/MainActivity.java
+++ app/src/main/java/com/tcjk/application/MainActivity.java
@@ -29,10 +29,4 @@ public class MainActivity extends Activity {
 
         Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
     }
-
-    @OnClick(R.id.btn)
-    public void onBtnClicked() {
-
-        Toast.makeText(this, "btnclick", Toast.LENGTH_SHORT).show();
-    }
 }
